56f91d38d03ee132036d55dd41757592a9f792dd
dgnajera <dgnajera2@172.19.156.45>
2016-12-09 00:23:34 -0700

    Added checkUserReadAccess()

--- ACCESS/src/User.java
+++ ACCESS/src/User.java
@@ -1,4 +1,11 @@
-
 public class User {
+    private Role[] roles;
+    private String userType;
 
+    boolean checkUserReadAccess(int ID) {
+	for (int i = 0; i < this.roles.length; i++)
+	    if (this.roles[i].getID() == ID && this.roles[i].hasReadAccess())
+		return true;
+	return false;
+    }
 }
