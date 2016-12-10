160b50144bf203befc29ec9b49439829fdc072ad
dgnajera <dgnajera2@172.19.156.45>
2016-12-09 20:12:47 -0700

    Added CRUD functions

--- ACCESS/src/InitiativeQuery.java
+++ ACCESS/src/InitiativeQuery.java
@@ -6,4 +6,60 @@ public class InitiativeQuery extends Query{
 		ds = new Database();
 		ds.connect();
 	}
+
+	public ResultSet getInitative(String title){
+		Statement stmt = null;
+		try {
+			stmt = ds.con.createStatement();
+			String sql = "SELECT * FROM " + table + " WHERE Title = "+title; 
+			ResultSet rs = stmt.executeQuery(sql);
+			return rs;
+
+		}catch (SQLException e){
+			e.printStackTrace();
+		}
+		return null;
+	}
+	
+	public void deleteInitiative(String title){
+		Statement stmt = null;
+		try{
+			stmt = ds.con.createStatement();
+			String sql = "DELETE FROM "+table+" WHERE Title = "+title;
+			stmt.executeQuery(sql);
+		}catch(SQLException e){
+			e.printStackTrace();
+		}
+	}
+	
+	public void updateInitiative(Initiative i){
+		Statement stmt = null;
+		try{
+			stmt = ds.con.createStatement();
+			String sql = "UPDATE "+table+" SET Title="+i.getTitle()+",Description="+i.getDescription()
+			+",StartDate="+i.getStartDate()+",EndDate="+i.getEndDate()+",Attachment="+i.getAttachment()
+			+",Website="+i.getWebsite()+",AssocPID="+i.getAssocPID()+",AssocActID="+i.getAssocActID()
+			+",Membership="+i.getMemberships();
+			stmt.executeQuery(sql);
+		}catch(SQLException e){
+			e.printStackTrace();
+		}
+		
+	}
+	
+	public void createInitiative(Initiative i){
+		Statement stmt = null;
+		try{
+			stmt = ds.con.createStatement();
+			String sql = "INSERT INTO initiatve(Title, Description, StartDate, EndDate, Goal,"
+			+ " Attachment, Website, AssocPID, AssocActID, Membership, Membership) "
+			+ "VALUES("+i.getTitle()+","+i.getDescription()+","+i.getStartDate()+","
+			+i.getEndDate()+","+i.getAttachment()+","+i.getWebsite()+","+i.getAssocPID()+","
+			+i.getAssocActID()+","+i.getMemberships()+")";
+			
+			stmt.executeQuery(sql);
+		}catch(SQLException e){
+			e.printStackTrace();
+		}
+	}
 }
